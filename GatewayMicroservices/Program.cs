911ab781a229f2f66b97181a99980ea29da4a5ff
MACTEPwar <shebanic95@gmail.com>
2023-11-19 16:50:38 +0200

    fix

--- GatewayMicroservices/Program.cs
+++ GatewayMicroservices/Program.cs
@@ -52,11 +52,8 @@ app.UseCors(options =>
 });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
